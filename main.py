7b9761aa55aebc4e01f7372569a45987779ec050
AnQiYAN <781236437@qq.com>
2021-12-30 14:04:10 +0800

    added more test

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# content of test_sample.py
 def inc(x):
-    print("This one is a new line")
-    print("Test")
     return x + 1
 
 
 def test_answer():
-    print("This one is a new line")
     assert inc(3) == 3
 
+def test_result():
+    assert inc(2) == 3
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
