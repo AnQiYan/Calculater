3ad0524244744f0587c414cfa2cc3ac36a917a5f
AnQiYAN <781236437@qq.com>
2021-12-30 15:13:40 +0800

    Test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-class HelloWorld():
-    def __init__(self, name):
-        self.name = name
+def inc(x):
+    return x + 1
 
-    def printname(self):
-        print(self.name)
+def test_answer():
+    assert inc(3) == 3
+
+def test_result():
+    assert inc(2) == 3
