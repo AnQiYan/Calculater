0a696092962d3458be1a0f806c81755578a60a15
AnQiYAN <781236437@qq.com>
2021-12-30 13:22:42 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
